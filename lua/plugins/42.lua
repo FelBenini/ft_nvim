80bc1325c11d73e74b58dda7e628baca06393f61
felbenini <112193194+FelBenini@users.noreply.github.com>
2025-11-24 18:08:58 -0300

    fix: changed typo on lua/plugins, mail property was wrong

--- lua/plugins/42.lua
+++ lua/plugins/42.lua
@@ -7,7 +7,7 @@ return {
       default_map = true,
       auto_update = true,
       user = "fbenini-", -- Your intra username
-      mailas = "fbenini-@student.42sp.org.br", -- Your 42 email
+      mail = "fbenini-@student.42sp.org.br", -- Your 42 email
     },
   },
 }
